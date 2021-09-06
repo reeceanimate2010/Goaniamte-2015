91fb8da34f1e61fcdb62c967809d2ab3e7568c8b
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-05 21:16:01 -0400

    Update page.ts

--- static/page.ts
+++ static/page.ts
@@ -42,8 +42,16 @@ module.exports = function (req, res, url) {
 				data: process.env.SWF_URL + "/go_full.swf",
 				type: "application/x-shockwave-flash",
 				id: "Studio",
+                                swf: process.env.SWF_URL + "/go_full.swf",
                                 width: "100%",
-                                height: "100%",  
+                                height: "100%",
+
+                                align: "middle",
+                                allowScriptAccess: "always",
+                                allowFullScreen: "true",
+                                wmode: "window",
+
+                                hasVersion: "10.3",
 			};
 			params = {
 				flashvars: {
@@ -98,8 +106,7 @@ module.exports = function (req, res, url) {
 					newusr: 1,
 					goteam_draft_only: 0,
 				},
-				allowScriptAccess: "always",
-				movie: process.env.SWF_URL + "/go_full.swf",
+				movie: process.env.SWF_URL + "/go_full.swf"
 			};
 			break;
 		}
