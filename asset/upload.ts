010bc49fe1d112cd028b8ee35b12d8b3a5477c60
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-06 01:54:49 -0400

    Update upload.ts

--- asset/upload.ts
+++ asset/upload.ts
@@ -11,45 +11,16 @@ const fs = require("fs");
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	if (req.method != "POST") return;
-	switch (url.pathname) {
-		case "/ajax/saveUserProp":
-			formidable().parse(req, (_, fields, files) => {
-				var [mId, mode, ext] = fields.params;
-				switch (mode) {
-					case "voiceover":
-						mode = "voiceover";
-						break;
-					case "soundeffect":
-						mode = "soundeffect";
-						break;
-					case "bgmusic":
-						mode = "bgmusic";
-						break;
-					case "bg":
-						mode = "background";
-						break;
-				}
+	if (req.method != 'POST' || url.path != '/ajax/saveUserProp') return;
+	new formidable.IncomingForm().parse(req, (e, f, files) => {
+		const path = files.import.path, buffer = fs.readFileSync(path);
+		const mId = loadPost.get(req).movieId;
 
-				var path = files.import.path;
-				var buffer = fs.readFileSync(path);
-				asset.save(buffer, mId, mode, ext);
-				fs.unlinkSync(path);
-				delete buffer;
-				res.end();
-			});
-			return true;
-		case "/goapi/saveSound/":
-			loadPost(req, res).then(([data, mId]) => {
-				var bytes = Buffer.from(data.bytes, "base64");
-				asset.save(bytes, mId, "voiceover", "ogg");
-			});
-			return true;
-		case "/goapi/saveTemplate/":
-			loadPost(req, res).then(([data, mId]) => {
-				var body = Buffer.from(data.body_zip, "base64");
-				res.end("0" + asset.save(body, mId, "starter", "xml"));
-			});
-			return true;
-	}
-};
+		const name = files.import.name;
+		const suffix = name.substr(name.lastIndexOf('.'));
+		asset.saveLocal(buffer, mId, suffix);
+		fs.unlinkSync(path);
+		res.end();
+	});
+	return true;
+}
